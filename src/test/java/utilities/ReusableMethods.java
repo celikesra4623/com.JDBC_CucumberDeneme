d4f7205b92f387c8979b9ac7881402cdabfec655
Selcuk <selcukemnn@gmail.com>
2024-06-13 17:38:31 +0300

    selcuk

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -5,49 +5,45 @@ import java.sql.*;
 public class ReusableMethods {
 
 
-      public static Connection connection ;
+    public static Connection connection;
+
+    public static Statement statement;
 
 
     public static void main(String[] args) throws SQLException {
-
         //Connection
-        Connection connection= DriverManager.getConnection(ConfigReader.getProperty("URL"),
-                                                           ConfigReader.getProperty("USERNAME"),
-                                                           ConfigReader.getProperty("PASSWORD"));
+        Connection connection = DriverManager.getConnection(ConfigReader.getProperty("URL"),
+                ConfigReader.getProperty("USERNAME"),
+                ConfigReader.getProperty("PASSWORD"));
 
         //Statement + SQL QUERY
 
-        Statement statement=connection.createStatement();
+        Statement statement = connection.createStatement();
 
-        String SQL="";
+        String SQL = "";
 
-        ResultSet resultSet=statement.executeQuery(SQL);
+        ResultSet resultSet = statement.executeQuery(SQL);
 
 
     }
 
-    public static Connection getConnection()  {
+    public static Connection getConnection() {
         try {
-
-             connection= DriverManager.getConnection(ConfigReader.getProperty("URL"),
+            connection = DriverManager.getConnection(ConfigReader.getProperty("URL"),
                     ConfigReader.getProperty("USERNAME"),
                     ConfigReader.getProperty("PASSWORD"));
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-
-
         }
-
-       return  connection;
+        return connection;
     }
 
-
-
-
+    public static Statement getStatement() {
+        try {
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return (statement);
     }
-
-
-
-
-
-
+}
