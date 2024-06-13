df9fcad4fa6577a44848f44b138f1de2c99cef08
damragencc <damragenc@gmail.com>
2024-06-13 17:34:19 +0300

    v1

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,4 +1,53 @@
 package utilities;
 
+import java.sql.*;
+
 public class ReusableMethods {
-}
+
+
+      public static Connection connection ;
+
+
+    public static void main(String[] args) throws SQLException {
+
+        //Connection
+        Connection connection= DriverManager.getConnection(ConfigReader.getProperty("URL"),
+                                                           ConfigReader.getProperty("USERNAME"),
+                                                           ConfigReader.getProperty("PASSWORD"));
+
+        //Statement + SQL QUERY
+
+        Statement statement=connection.createStatement();
+
+        String SQL="";
+
+        ResultSet resultSet=statement.executeQuery(SQL);
+
+
+    }
+
+    public static Connection getConnection()  {
+        try {
+
+             connection= DriverManager.getConnection(ConfigReader.getProperty("URL"),
+                    ConfigReader.getProperty("USERNAME"),
+                    ConfigReader.getProperty("PASSWORD"));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+
+        }
+
+       return  connection;
+    }
+
+
+
+
+    }
+
+
+
+
+
+
